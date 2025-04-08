a27cdc61513f48e396041370a3ddaeabd357fbc2
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2025-04-08 22:31:10 +0330

    Add project files.

---
+++ WerewolfAchievementViewerTests/Werewolf/ExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Text;
+using System.Text.Json;
+
+namespace WerewolfAchievementViewer.Werewolf.Tests;
+
+[TestClass]
+public class ExtensionsTests
+{
+    [TestMethod]
+    public void SetTest()
+    {
+        var bytes = new BitArray(200)
+            .Set(AchievementId.Inconspicuous)
+            .Set(AchievementId.IHelped)
+            .Set(AchievementId.Firefighter)
+            .ToByteArray();
+
+        //if (BitConverter.IsLittleEndian)
+        //    Array.Reverse(bytes);
+
+        var result = JsonSerializer.Serialize(bytes);
+        var url = $"https://localhost:7024/achs/{result}";
+        Console.WriteLine(url);
+    }
+}

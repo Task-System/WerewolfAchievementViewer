9377a111d16899b76d4e3f54ef5eb2727210d93c
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2025-04-18 13:36:27 +0330

    ...

--- WerewolfAchievementViewer/Werewolf/CustomWerewolfRequest.cs
+++ WerewolfAchievementViewer/Werewolf/CustomWerewolfRequest.cs
@@ -1,47 +1,10 @@
 ﻿using NewTsWw.Models.Werewolf;
 using System.Net;
-using System.Net.Http.Json;
-using System.Text.Json;
 
 namespace WerewolfAchievementViewer.Werewolf;
 
-public class CustomWerewolfRequest : IDisposable
+public class CustomWerewolfRequest() : WerewolfRequest(
+    new($"https://corsproxy.io/?url={WebUtility.UrlEncode("https://www.tgwerewolf.com/Stats")}"))
 {
-    private readonly HttpClient _httpClient;
 
-    public CustomWerewolfRequest()
-    {
-        _httpClient = new HttpClient
-        {
-            Timeout = TimeSpan.FromSeconds(5),
-            BaseAddress = new Uri("https://www.tgwerewolf.com/Stats")
-        };
-    }
-
-    public Task<TValue?> GetEndpoint<TValue>(
-        WerewolfRequestEndpoint endpoint, long userId)
-            => _httpClient.GetFromJsonAsync<TValue>(
-                $"https://corsproxy.io/?url={WebUtility.UrlEncode(_httpClient.BaseAddress + $"/{endpoint}/?pid={userId}&json=true")}");
-
-    public async Task<AchievementInfo[]?> GetAchievements(long userId)
-    {
-        try
-        {
-            return await GetEndpoint<AchievementInfo[]>(WerewolfRequestEndpoint.PlayerAchievements, userId);
-        }
-        catch (HttpRequestException he)
-        {
-            return null;
-        }
-        catch (JsonException je)
-        {
-            return [];
-        }
-    }
-
-    public void Dispose()
-    {
-        GC.SuppressFinalize(this);
-        _httpClient.Dispose();
-    }
 }
